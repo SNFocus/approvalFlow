6c34326cfaf5d57a9010d7384b0e84651a0e9b74
Dengsuning <deng_suning@163.com>
2020-05-22 19:03:48 +0800

    设置axios请求资源为相对路径

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import { get } from '@/axios'
 // 获取组织机构根节点
-export const GET_DEPT_ROOT = () => get( '/depRoot.json' )
+export const GET_DEPT_ROOT = () => get( './depRoot.json' )
 
 // 根据部门id获取分页人员信息
-export const GET_PAGE_EMPLOYEE = data => get( '/userData.json', data )
+export const GET_PAGE_EMPLOYEE = data => get( './userData.json', data )
 
 // 获取组织机构子节点
-export const GET_DEPT_TREE = data => get( '/depChild.json', data )
+export const GET_DEPT_TREE = data => get( './depChild.json', data )
 
 // 获取组织机构下人员信息
-export const GET_USER_BY_DEPT = data => get( '/userData.json', data )
+export const GET_USER_BY_DEPT = data => get( './userData.json', data )
