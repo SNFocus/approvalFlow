ab3bf14db865a2d149f649ff55a1596e0f194e6f
Dengsuning <deng_suning@163.com>
2020-05-23 13:56:59 +0800

    表格中组件不能作为流程条件

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,13 @@
 import { get } from '@/axios'
+const BASE_URL = process.env.BASE_URL
 // 获取组织机构根节点
-export const GET_DEPT_ROOT = () => get( '/approvalflow/depRoot.json' )
+export const GET_DEPT_ROOT = () => get( BASE_URL + 'depRoot.json' )
 
 // 根据部门id获取分页人员信息
-export const GET_PAGE_EMPLOYEE = data => get( '/approvalflow/userData.json', data )
+export const GET_PAGE_EMPLOYEE = data => get( BASE_URL + 'userData.json', data )
 
 // 获取组织机构子节点
-export const GET_DEPT_TREE = data => get( '/approvalflow/depChild.json', data )
+export const GET_DEPT_TREE = data => get( BASE_URL + 'depChild.json', data )
 
 // 获取组织机构下人员信息
-export const GET_USER_BY_DEPT = data => get( '/approvalflow/userData.json', data )
+export const GET_USER_BY_DEPT = data => get( BASE_URL + 'userData.json', data )
